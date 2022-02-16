92a800e4245acb66b7a75a54fe4f320ffb7ac774
Lucas Rocha <lucas.rocha@maida.health>
2022-02-15 23:18:08 -0300

    MA-1 Adicionando Fields ao usuario e a gem Enum para cadastrar ADM

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable :confirmable
+
+  enum role: { admin: 0, otaku: 1}
+
+
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 end
