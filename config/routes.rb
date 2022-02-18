48c18ffe3b7f13e1d51e34f8c7b7c3497af5b8a0
Lucas Rocha <lucas.rocha@maida.health>
2022-02-18 15:57:16 -0300

    MA-3 Adicionando model de Follow e as referencias a usuario e anime

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'follows/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'animes#index'
+  resources :follows
   resources :animes
   resources :users
 end
