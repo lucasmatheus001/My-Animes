6a3ac04761260a3c7509d9b6a5bc41f8579c8439
Lucas Rocha <lucas.rocha@maida.health>
2022-02-21 23:50:16 -0300

    MA-9 Atualizando seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u = User.new(name:"Lucas", email: "admin@admin.com", password: "12345678",password_confirmation: "12345678", role: :admin)
+u = User.new(name:"Lucas", email: "admin4@admin.com", password: "12345678",password_confirmation: "12345678", role: :admin)
 u.save!
 
-o = User.new(name:"Hugo", email: "otaku@otaku.com", password: "12345678",password_confirmation: "12345678", role: :otaku)
+o = User.new(name:"Hugo", email: "otaku444@otaku.com", password: "12345678",password_confirmation: "12345678", role: :otaku)
 o.save
 
-anime1 = Anime.new(name: "One Piece", description: "Gol D. Roger was known as the "Pirate King," the stron
-    gest and most infamous being to have sailed the Grand Line. The capture and execution of Roger by the World Govern
-    ment brought a change throughout the world. His last words before his death revealed the existence of the greatest
-     treasure in the world, One Piece. It was this revelation that brought about the Grand Age of Pirates, men who dre
-    amed of finding One Piece—which promises an unlimited amount of riches and fame—and quite possibly the pinnacle of
-     glory and the title of the Pirate King. Enter Monkey D. Luffy, a 17-year-old boy who defies your standard definit
-    ion of a pirate",image_url:"https://cdn.myanimelist.net/images/anime/6/73245.jpg",genres: "Acao e aventura",episod
-    es: 300,studios: "Toey Animation",duration: 21, situation: "Em andamento",user_id: 2)
+anime1 = Anime.new(name: "One Piece", description: "Gol D. Roger was known as the Pirate King, the stron
+gest and most infamous being to have sailed the Grand Line. The capture and execution of Roger by the World Govern ment brought a change throughout the world. His last words before his death revealed the existence of the greatest treasure in the world, One Piece. It was this revelation that brought about the Grand Age of Pirates, men who dreamed of finding One Piece—which promises an unlimited amount of riches and fame—and quite possibly the pinnacle of glory and the title of the Pirate King. Enter Monkey D. Luffy, a 17-year-old boy who defies your standard definition of a pirate",image_url:"https://cdn.myanimelist.net/images/anime/6/73245.jpg",genres: "Acao e aventura",episodes: 300,studios: "Toey Animation",duration: 21, situation: "Em andamento",user_id: 2)
 
 anime1.save
 
-anime2 = Anime.new(name: "Youjo Senki II", description: ""Saga of Tanya the Evil, ou no Japão Yōjo Senki (幼女戦記?, lit. \"Crônicas Militares de uma Pequena Garota\"), é uma light novel japonesa escrita por Carlo Zen e ilustrada por Shinobu Shinotsuki. A publicadora Enterbrain publicou doze volumes a partir de 31 de outubro de 2013. Uma adaptação para mangá com arte de Chika Tōjō começou a serialização em Kadokawa Shoten Comp Ace Magazine em 26 de abril de 2016. Uma adaptação de anime pela NUT estreou em 6 de janeiro de 2017."",image_url:"https://cdn.myanimelist.net/images/anime/1437/115925.jpg",genres: "Acao e aventura",episodes
-: 500,studios: "Toey Animation",duration: 21, situation: "Em andamento",user_id: 2)
+anime2 = Anime.new(name: "Youjo Senki II", description: "Saga of Tanya the Evil, ou no Japão Yōjo Senki (幼女戦記?, lit. \"Crônicas Militares de uma Pequena Garota\"), é uma light novel japonesa escrita por Carlo Zen e ilustrada por Shinobu Shinotsuki. A publicadora Enterbrain publicou doze volumes a partir de 31 de outubro de 2013. Uma adaptação para mangá com arte de Chika Tōjō começou a serialização em Kadokawa Shoten Comp Ace Magazine em 26 de abril de 2016. Uma adaptação de anime pela NUT estreou em 6 de janeiro de 2017."",image_url:"https://cdn.myanimelist.net/images/anime/1437/115925.jpg",genres: "Acao e aventura",episodes: 500,studios: "Toey Animation",duration: 21, situation: "Em andamento",user_id: 2)
 anime2.save
 
-anime3 = Anime.new(name: "Shingeki no Kyojin", description: "Turning against his former allies and enemies alike, Eren Yeager sets a disastrous plan in motion. Under the guidance of the Beast Titan, Zeke, Eren takes extreme measures to end the ancient conflict between Marley and Eldia—but his true intentions remain a mystery. Delving deep into his family's past, Eren fights to control his own destiny. Meanwhile, the long-feuding nations of Marley and Eldia utilize both soldiers and Titans in a brutal race to eliminate the other. Reiner Braun uses his own powers in a desperate bid to hold off Eren's own militaristic force, and his fellow Eldians—children Falco Grice and Gabi Braun—struggle to survive in the unfolding chaos.",image_url:"https://cdn.myanimelist.net/images/anime/1948/120625.jpg",genres: "Acao e aventura",episodes: 245,studios: "Toey Animation",duration: 21, situation: "Em andamento",user_id: 2)
+anime3 = Anime.new(name: "Shingeki no Kyojin", description: "Turning against his former allies and enemies alike, Eren Yeager sets a disastrous plan in motion. Under the guidance of the Beast Titan, Zeke, Eren takes extreme measures to end the ancient conflict between Marley and Eldia—but his true intentions remain a mystery. Delving deep into his familys past, Eren fights to control his own destiny. Meanwhile, the long-feuding nations of Marley and Eldia utilize both soldiers and Titans in a brutal race to eliminate the other. Reiner Braun uses his own powers in a desperate bid to hold off Erens own militaristic force, and his fellow Eldians—children Falco Grice and Gabi Braun—struggle to survive in the unfolding chaos.",image_url:"https://cdn.myanimelist.net/images/anime/1948/120625.jpg",genres: "Acao e aventura",episodes: 245,studios: "Toey Animation",duration: 21, situation: "Em andamento",user_id: 2)
 anime3.save
 
 anime4 = Anime.new(name: "One Piece", description: "Gol D. Roger was known as the Pirate King, the strongest and most infamous being to have sailed the Grand Line. The capture and execution of Roger by the World Government brought a change throughout the world. His last words before his death revealed the existence of the greatest treasure in the world, One Piece. It was this revelation that brought about the Grand Age of Pirates, men who dreamed of finding One Piece—which promises an unlimited amount of riches and fame—and quite possibly the pinnacle of glory and the title of the Pirate King. Enter Monkey D. Luffy, a 17-year-old boy who defies your standard definition of a pirate",image_url:"https://cdn.myanimelist.net/images/manga/2/253146.jpg",genres: "Acao e aventura",episodes: 1005,studios: "Toey Animation",duration: 21, situation: "Em andamento",user_id: 2)
