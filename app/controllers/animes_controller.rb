05eb93fec7dcdbc2cbf519ad5519aa6f82c240e0
lucasmatheus001 <lucasmatheussm22@gmail.com>
2024-02-13 21:36:03 -0300

    Adicionando metodo para api

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -2,7 +2,27 @@ class AnimesController < ApplicationController
   before_action :set_anime, only: %i[ show edit update destroy ]
 
   def index
-      @animes = current_user.admin? ?  Anime.order(id: :desc) : current_user.animes.where(status: "ativo")
+    # @animes = current_user.admin? ?  Anime.order(id: :desc) : current_user.animes.where(status: "ativo")
+    @api_service = AnimeService.new
+    @animes = Anime.all
+    # @resultados = @api_service.obter_recurso
+    @detalhes_animes = obter_detalhes_para_cada_anime(@animes)
+
+    begin
+
+      @anime_data = api_service
+      puts @anime_data.class
+      # Lógica para lidar com o recurso aqui
+
+
+      render json: recurso
+    rescue => e
+      # Lidar com erros, talvez renderizar uma mensagem de erro
+      flash[:error] = "Erro ao obter dados da API: #{e.message}"
+      # render json: { erro: e.message }, status: :unprocessable_entity
+    end
+
+    render 'index'
   end
 
   def all
@@ -11,6 +31,35 @@ class AnimesController < ApplicationController
 
   def show
       @animes = Anime.all
+  end
+
+  def busca
+    api_service = AnimeService.new
+
+    begin
+      q = params[:q]
+      @resultados = api_service.buscar_animes(q)
+      redirect_to show_from_json_animes_path(anime: dados_json)
+    rescue => e
+      flash[:error] = "Erro ao realizar a busca: #{e.message}"
+    end
+
+    render 'busca'
+  end
+
+  def show_from_json
+    @anime_data = JSON.parse(params[:anime], symbolize_names: true)
+  end
+
+  def cadastrar_anime_from_json
+    @anime_data = JSON.parse(params[:anime], symbolize_names: true)
+    @anime = Anime.new(
+
+      anime_id: @anime_data[:mal_id],
+      user_id: current_user.id
+    )
+    @anime.save
+    render 'show_from_json'
   end
 
   def result
@@ -64,6 +113,16 @@ class AnimesController < ApplicationController
   def set_anime
       @anime = Anime.find(params[:id])
       # authorize @user
+  end
+
+  def obter_detalhes_para_cada_anime(animes)
+    detalhes_animes = []
+    @animes2 = animes
+    @animes2.each do |anime|
+      detalhes_animes << @api_service.obter_detalhes_do_anime(anime.anime_id)
+
+    end
+    detalhes_animes
   end
 
   def anime_params
--- db/migrate/20220218030823_create_animes.rb
+++ db/migrate/20220218030823_create_animes.rb
@@ -1,16 +1,8 @@
 class CreateAnimes < ActiveRecord::Migration[6.1]
   def change
     create_table :animes do |t|
+      t.string "anime_id"
       t.references :user, null: false, foreign_key: true
-      t.string :name, null: false
-      t.string :description, null: false
-      t.string :image_url, null: false
-      t.string :genres, null: false
-      t.integer :episodes, null: false
-      t.integer :status, default: 0
-      t.string :studios, null: false
-      t.integer :duration, null: false
-      t.string :situation, null: false
       t.timestamps
     end
   end
